89abb891ee7963fea3df7a963857f55c069f424d
Arnau Clavijos <arnau.clavijos@launchmetrics.com>
2024-07-11 09:45:10 +0200

    Removed Authorization Part

--- expressServer/index.js
+++ expressServer/index.js
@@ -42,31 +42,13 @@ app.post('/api/login', async (req, res) => {
     }
 
     const token = generateJWT(user);
+
     res.status(200).json({ message: 'Login successful', token });
   } catch (error) {
     console.error('Error during login:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
-
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
-  }
-};
 
 app.get('/api/snakes', authenticateJWT, async (_, res) => {
   try {
